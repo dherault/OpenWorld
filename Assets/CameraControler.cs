b15fab878804e1dcdaeac94d054ad9a9df8c6a70
David Hérault <dherault@gmail.com>
2019-12-19 22:55:03 +0100

    End of camera tryouts

--- Assets/CameraControler.cs
+++ Assets/CameraControler.cs
@@ -6,14 +6,16 @@ using UnityEngine;
 public class CameraControler : MonoBehaviour
 {
 
-  private float moveSpeed = 0.5f;
-  private float scrollSpeed = 10f;
+  private float moveSpeed = 1.5f;
+  private float scrollSpeed = 10;
   private float orbitSensibility = 0.23f;
   private Vector3 previousMousePosition = new Vector3(0, 0, 0);
   private double[,] rotationMatrix = new double[3, 3];
+  private float goalHeight = 10;
+  private float heightIncrement = 0.5f;
 
   void Start() {
-    transform.position = new Vector3(0, 10, 0);
+    transform.position = new Vector3(0, goalHeight, 0);
   }
 
   void Update () {
@@ -26,27 +28,38 @@ public class CameraControler : MonoBehaviour
 
     previousMousePosition = Input.mousePosition;
 
-
-
     if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
-      // InitializeRotationMatrix();
+      InitializeRotationMatrix();
 
-      // rotationMatrix[1, 1] = 1;
-      // rotationMatrix[0, 0] = Math.Cos(transform.eulerAngles.y);
-      // rotationMatrix[0, 1] = Math.Sin(transform.eulerAngles.y);
-      // rotationMatrix[2, 0] = -Math.Sin(transform.eulerAngles.y);
-      // rotationMatrix[2, 1] = Math.Cos(transform.eulerAngles.y);
+      rotationMatrix[1, 1] = 1;
+      rotationMatrix[0, 0] = Math.Cos(transform.eulerAngles.y * Math.PI / 180);
+      rotationMatrix[0, 2] = Math.Sin(transform.eulerAngles.y * Math.PI / 180);
+      rotationMatrix[2, 0] = -Math.Sin(transform.eulerAngles.y * Math.PI / 180);
+      rotationMatrix[2, 2] = Math.Cos(transform.eulerAngles.y * Math.PI / 180);
 
       Vector3 velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-      transform.position += moveSpeed * MultiplyVectorAndMatrix(velocity, rotationMatrix);
-      transform.position += moveSpeed * velocity;
+      transform.position += moveSpeed * MultiplyMatrixAndVector(rotationMatrix, velocity);
     }
 
     if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-      transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+      goalHeight += scrollSpeed * -Input.GetAxis("Mouse ScrollWheel");
     }
 
+    if (transform.position.y != goalHeight) {
+      float diff = goalHeight - transform.position.y;
+      float diffSign = Math.Sign(diff);
+
+      diff = Math.Min(heightIncrement, Math.Abs(diff));
+
+      transform.position += new Vector3(0, diffSign * diff, 0);
+    }
+
+    transform.position =new Vector3(
+      Math.Min(50, Math.Max(-50, transform.position.x)),
+      Math.Min(20, Math.Max(1, transform.position.y)),
+      Math.Min(50, Math.Max(-50, transform.position.z))
+    );
   }
 
   void InitializeRotationMatrix() {
@@ -57,12 +70,11 @@ public class CameraControler : MonoBehaviour
     }
   }
 
-  Vector3 MultiplyVectorAndMatrix(Vector3 v, double[,] m) {
+  Vector3 MultiplyMatrixAndVector(double[,] m, Vector3 v) {
     return new Vector3(
-      v.x * (float)m[0, 0] + v.y * (float)m[1, 0] + v.z * (float)m[2, 0],
-      v.x * (float)m[0, 1] + v.y * (float)m[1, 1] + v.z * (float)m[2, 1],
-      v.x * (float)m[0, 2] + v.y * (float)m[1, 2] + v.z * (float)m[2, 2]
+      v.x * (float)m[0, 0] + v.y * (float)m[0, 1] + v.z * (float)m[0, 2],
+      v.x * (float)m[1, 0] + v.y * (float)m[1, 1] + v.z * (float)m[1, 2],
+      v.x * (float)m[2, 0] + v.y * (float)m[2, 1] + v.z * (float)m[2, 2]
     );
   }
-
 }
