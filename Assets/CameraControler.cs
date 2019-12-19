9199631e23fd93ce250b449fa7fe1fd7a4a65130
David Hérault <dherault@gmail.com>
2019-12-19 17:38:11 +0100

    Create terrain

---
+++ Assets/CameraControler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+
+  private float mainSpeed = 25.0f;
+  private float orbitSensibility = 0.25f;
+  private float panSensibility = 0.1f;
+  private Vector3 mousePosition = new Vector3(0, 0, 0);
+  private Vector3 mousePositionDifference;
+  private Vector3 v;
+
+  void Start() {
+    transform.position = new Vector3(0, 10, 0);
+  }
+
+  void Update () {
+    mousePositionDifference = Input.mousePosition - mousePosition;
+
+    if (Input.GetMouseButton(1)) {
+      v = new Vector3(-mousePositionDifference.y * orbitSensibility, mousePositionDifference.x * orbitSensibility, 0);
+
+      transform.eulerAngles = new Vector3(transform.eulerAngles.x + v.x , transform.eulerAngles.y + v.y, 0);
+    }
+
+    v = GetBaseInput();
+
+    if (Input.GetMouseButton(2)) {
+      v -= mousePositionDifference * panSensibility;
+    }
+
+    v *= mainSpeed * Time.deltaTime;
+    mousePosition = Input.mousePosition;
+
+    transform.Translate(v);
+    transform.position = new Vector3(
+      Math.Max(-50, Math.Min(50, transform.position.x)),
+      Math.Max(3, Math.Min(20, transform.position.y)),
+      Math.Max(-50, Math.Min(50, transform.position.z))
+    );
+  }
+
+  private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
+    Vector3 speed = new Vector3();
+
+    if (Input.GetKey (KeyCode.W)) {
+      speed += new Vector3(0, 0 , 1);
+    }
+
+    if (Input.GetKey (KeyCode.S)) {
+      speed += new Vector3(0, 0, -1);
+    }
+
+    if (Input.GetKey (KeyCode.A)) {
+      speed += new Vector3(-1, 0, 0);
+    }
+
+    if (Input.GetKey (KeyCode.D)) {
+      speed += new Vector3(1, 0, 0);
+    }
+
+    return speed;
+  }
+
+}
---
+++ Assets/CreateTerrain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTerrain : MonoBehaviour {
+
+  private int widthInVexels = 100;
+  private int heightInVexels = 100;
+
+  void Start() {
+    gameObject.SetActive(false);
+
+    Debug.Log("Start");
+
+    for (int x = -widthInVexels / 2; x < widthInVexels / 2; x++) {
+      for (int z = -heightInVexels / 2; z < heightInVexels / 2; z++) {
+        CreateCube(x, 0, z);
+      }
+    }
+
+  }
+
+  void Update() {
+
+  }
+
+  void CreateCube(int x, int y, int z) {
+    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    cube.transform.position = new Vector3(x, y, z);
+
+    var cubeRenderer = cube.GetComponent<Renderer>();
+
+    cubeRenderer.material.color = Color.red;
+  }
+
+}
