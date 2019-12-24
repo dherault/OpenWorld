b7fdd70ec74811597a57a7b2781e242e22571781
David Hérault <dherault@gmail.com>
2019-12-24 12:49:27 +0100

    Add drag zone

--- Assets/Scenes/WorldScene/UI/UIShortcutsController.cs
+++ Assets/Scenes/WorldScene/UI/UIShortcutsController.cs
@@ -16,14 +16,26 @@ public class UIShortcutsController : MonoBehaviour, Observer {
     Canvas canvas = gameObject.GetComponent<Canvas>();
     canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 
-    GameObject imageGameObject = new GameObject();
-    imageGameObject.name = "UIShortcutsCanvas-image";
-    imageGameObject.transform.SetParent(canvas.transform);
+    GameObject backgroundImageGameObject = new GameObject();
+    backgroundImageGameObject.name = "UIShortcutsCanvas-backgroundImage";
+    backgroundImageGameObject.transform.SetParent(canvas.transform);
 
-    Image image = imageGameObject.AddComponent<Image>();
-    image.color = new Color(255, 255, 255);
+    Image backgroundImage = backgroundImageGameObject.AddComponent<Image>();
+    backgroundImage.color = new Color(255, 255, 255);
 
-    image.rectTransform.sizeDelta = new Vector2(Screen.width * widthRatio, Screen.height * heightRatio);
+    backgroundImage.rectTransform.sizeDelta = new Vector2(Screen.width * widthRatio, Screen.height * heightRatio);
+    backgroundImage.rectTransform.anchoredPosition = new Vector2(0, 0);
+
+    GameObject dragImageGameObject = new GameObject();
+    dragImageGameObject.name = "UIShortcutsCanvas-backgroundImage-dragImage";
+    dragImageGameObject.transform.SetParent(backgroundImageGameObject.transform);
+
+    Image dragImage = dragImageGameObject.AddComponent<Image>();
+    dragImage.color = new Color(0, 255, 0);
+
+    dragImage.rectTransform.sizeDelta = new Vector2(Screen.width * widthRatio, 50);
+    dragImage.rectTransform.anchoredPosition = new Vector2(0, Screen.height * heightRatio / 2 - 50 / 2);
+
   }
 
   void Start() {
