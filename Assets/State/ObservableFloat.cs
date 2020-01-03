e9f66bde3766ee21f1681e0e1356ea9413bc089e
David Hérault <dherault@gmail.com>
2020-01-03 10:21:06 +0100

    working demo

---
+++ Assets/State/ObservableFloat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObservableFloat {
+  private List<Observer> observers = new List<Observer>();
+  private float _value;
+  public float _ {
+    get {
+      return _value;
+    }
+    set {
+      _value = value;
+      Notify();
+    }
+  }
+
+  public ObservableFloat(float initialValue) {
+    _value = initialValue;
+  }
+
+  public void Notify() {
+    observers.ForEach(x => x.Update());
+  }
+
+  public void Subscribe(Observer observer) {
+    observers.Add(observer);
+  }
+
+  public void Unsubscribe(Observer observer) {
+    observers.Remove(observer);
+  }
+}
---
+++ Assets/State/Spells.cs
@@ -0,0 +1,43 @@
+﻿
+public struct Spell {
+  public string Name;
+  public float Range;
+  public Element Element;
+  public int MinDamage;
+  public int MaxDamage;
+  public int MinCriticalDamage;
+  public int MaxCriticalDamage;
+
+  public Spell(string name, float range, Element element, int minDamage, int maxDamage, int minCriticalDamage, int maxCriticalDamage) {
+    Name = name;
+    Range = range;
+    Element = element;
+    MinDamage = minDamage;
+    MaxDamage = maxDamage;
+    MinCriticalDamage = minCriticalDamage;
+    MaxCriticalDamage = maxCriticalDamage;
+  }
+}
+
+public class Spells {
+  
+  public static Spell SwordSlash = new Spell(
+    "Sword Slash", 
+    1f,
+    Element.None,
+    12,
+    16,
+    14,
+    18
+  );
+  
+  public static Spell Thunder = new Spell(
+    "Thunder",
+    8f,
+    Element.Air,
+    10,
+    14,
+    12,
+    16
+  );
+}
